22a2e4313379f60a244762cdb952f6d562bffb1c
Nima Maghooli <nima.maghooli@ut.ac.ir>
2024-04-17 12:45:19 +0330

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -272,5 +272,15 @@ window.onbeforeunload = () => {
     form.reset();
   }
 }
+
+
+// javascript
+window.onload = function() { 
+  var el = document.getElementById('g-recaptcha-response'); 
+  if (el) { 
+    el.setAttribute('required', 'required'); 
+  } 
+}
+
   
 })()
